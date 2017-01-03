bc4f482091dc9f41f36b1cd6dff47cb1cb44d6b0
Josh <joshuagreen118@gmail.com>
2017-01-03 14:48:07 -0500

    Initial upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    getLocation();
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    var now = new Date();
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    
+    var sunTimes = SunCalc.getTimes(now, lat, long);
+    var moonTimes = SunCalc.getMoonTimes(now, lat, long, false);
+    
+    var sunRise = sunTimes['sunrise'];
+    var sunSet = sunTimes['sunset'];
+    
+    var moonRise = moonTimes['rise'];
+    var moonset = moonTimes['set'];
+    
+    setInterval(function(){ sunRotate(sunRise, sunSet); }, 100);
+    setInterval(function(){ moonRotate(moonRise, moonset); }, 100);
+    
+    loadWeather(position.coords.latitude+','+position.coords.longitude);
+}
+
+function sunRotate(rise, set) {
+    var now = new Date();
+    var currentMinute = now - rise;
+    var daylight = (set  - rise) / 60000; // Minutes of Daylight
+    var sunSpeed = (daylight * 2) / 360;
+    var deg = (currentMinute / 60000) / sunSpeed;
+    $("#day").rotate(deg);
+}
+
+function moonRotate(rise, set){
+    var now = new Date();
+    var currentMinute = now - rise;
+    var moonlight = (set  - rise) / 60000; // Minutes of Moonlight
+    var moonSpeed = Math.abs((moonlight * 2) / 360);
+    var deg = (currentMinute / 60000) / moonSpeed;
+    console.log(moonSpeed);
+    $("#night").rotate(deg);
+}
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+document.getElementById("Email").value = "";
+document.getElementById("SSO").checked = true;
+document.getElementById("zall").value = "United States, Virginia, Richmond, Virginia Beach (EST)";
+document.getElementById("admin").checked = true;
+document.getElementById("TargetUCCAL").value = "128";
